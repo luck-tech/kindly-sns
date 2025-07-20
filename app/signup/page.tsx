7a1285aa84854707ab9bc2979205fd0881538a65
yuu-1230 <yuto.nagata1230@gmail.com>
2025-07-20 16:26:07 +0900

    ログイン・サインアップ画面完成

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,25 +4,30 @@ import { Button } from "@/components/ui/button";
 
 export default function Signup() {
   return (
-    <div className="w-full flex justify-center items-center px-[120px] py-20 font-hachimarupop">
-      <div className="p-6 mx-auto flex flex-col gap-4 max-w-[960px] h-[695px]">
-        <p className="pt-20 pb-12 px-16 text-[28px]">おかえりなさい</p>
+    <div className="w-full flex px-[160px] py-20 font-hachimarupop">
+      <div className="m-0 w-full flex flex-col h-[695px] justify-center items-center">
+        <p className="pt-[20px] pb-[12px] px-[16px] text-[28px] text-center ">
+          おかえりなさい
+        </p>
         <input
           type="email"
           name=""
           id=""
           placeholder="メールアドレス"
-          className="text-[16px]"
+          className="w-[448px] bg-[#F5F2F2] mx-[12px] my-[16px] p-[16px] rounded-[12px] placeholder:text-[16px] placeholder:text-[#827066]"
         />
         <input
           type="password"
           name=""
           id=""
           placeholder="パスワード"
-          className="text-[16px]"
+          className="w-[448px] bg-[#F5F2F2] mx-[12px] my-[16px] p-[16px] rounded-[12px] placeholder:text-[16px] placeholder:text-[#827066]"
         />
-        <Button variant="default">登録</Button>
-        <a href="/login" className="text-[14px]">
+        <Button variant="login_signup">登録</Button>
+        <a
+          href="/login"
+          className="pt-[4px] pb-[12px] px-[16px] text-[14px] text-[#827066]"
+        >
           既にアカウントをお持ちですか？ ログイン
         </a>
       </div>
