ff9403b5af86c27b6765155887639fff67e28bee
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-07-21 02:01:39 +0900

    feat: レビューの修正

--- components/header.tsx
+++ components/header.tsx
@@ -1,37 +1,45 @@
 import React from "react";
 import Image from "next/image";
-
-import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
+import Link from "next/link";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { hachiMaruPop } from "@/app/fonts";
 
 const Header = () => {
   return (
     <header className="fixed w-full h-16 px-6 py-2 bg-white flex justify-between border-b border-gray-200">
-      <Image
-        src="/logo.png"
-        alt=""
-        width={500}
-        height={500}
-        className="h-full w-auto aspect-square"
-      />
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={500}
+          height={500}
+          className="h-full w-auto aspect-square"
+        />
+      </Link>
       <Popover>
         <PopoverTrigger>
-          {" "}
           <Avatar>
             <AvatarImage
               src="https://github.com/shadcn.png"
               alt="プロフィール画像"
-              className="h-full w-auto aspect-square rounded-full"
+              className="h-full w-auto aspect-square cursor-pointer"
             />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
         </PopoverTrigger>
-        <PopoverContent>
-          <button className="text-red-500">ログアウト</button>
+        <PopoverContent className={hachiMaruPop.className}>
+          <button
+            className="text-red-500 cursor-pointer"
+            type="button"
+            aria-label="Logout"
+          >
+            ログアウト
+          </button>
         </PopoverContent>
       </Popover>
     </header>
