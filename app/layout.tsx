f46c93bb3e0dcf844facf5e95c31e2b4055040f1
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-07-20 17:15:58 +0900

    refactor: headerをHeaderコンポーネントに分離

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Image from "next/image";
-import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,31 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="w-full h-16 px-6 py-2 flex justify-between border-b border-gray-200">
-          <Image
-            src="/logo.png"
-            alt=""
-            width={500}
-            height={500}
-            className="h-full w-auto aspect-square"
-          />
-          <Popover>
-            <PopoverTrigger>
-              {" "}
-              <Avatar>
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="プロフィール画像"
-                  className="h-full w-auto aspect-square rounded-full"
-                />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </PopoverTrigger>
-            <PopoverContent>
-              <button className="text-red-500">ログアウト</button>
-            </PopoverContent>
-          </Popover>
-        </div>
+        <Header />
         {children}
       </body>
     </html>
