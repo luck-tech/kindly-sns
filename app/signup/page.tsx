116b3676731fb6542ad998546cb1931cd8139b08
yuu-1230 <yuto.nagata1230@gmail.com>
2025-07-20 15:08:56 +0900

    "hachimarupop"フォント追加

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,25 +4,25 @@ import { Button } from "@/components/ui/button";
 
 export default function Signup() {
   return (
-    <div className="w-full flex justify-center items-center px-[120px] py-20">
+    <div className="w-full flex justify-center items-center px-[120px] py-20 font-hachimarupop">
       <div className="p-6 mx-auto flex flex-col gap-4 max-w-[960px] h-[695px]">
-        <p className="pt-20 pb-12 px-16 ">おかえりなさい</p>
+        <p className="pt-20 pb-12 px-16 text-[28px]">おかえりなさい</p>
         <input
           type="email"
           name=""
           id=""
           placeholder="メールアドレス"
-          className=""
+          className="text-[16px]"
         />
         <input
           type="password"
           name=""
           id=""
           placeholder="パスワード"
-          className=""
+          className="text-[16px]"
         />
         <Button variant="default">登録</Button>
-        <a href="/login" className="">
+        <a href="/login" className="text-[14px]">
           既にアカウントをお持ちですか？ ログイン
         </a>
       </div>
