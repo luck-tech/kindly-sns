5ec15121de774bb9b5cf0a11c7519445b9f7c331
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-07-20 17:15:58 +0900

    refactor: headerをHeaderコンポーネントに分離

---
+++ components/header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "next/image";
+
+import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+const Header = () => {
+  return (
+    <div className="w-full h-16 px-6 py-2 flex justify-between border-b border-gray-200">
+      <Image
+        src="/logo.png"
+        alt=""
+        width={500}
+        height={500}
+        className="h-full w-auto aspect-square"
+      />
+      <Popover>
+        <PopoverTrigger>
+          {" "}
+          <Avatar>
+            <AvatarImage
+              src="https://github.com/shadcn.png"
+              alt="プロフィール画像"
+              className="h-full w-auto aspect-square rounded-full"
+            />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </PopoverTrigger>
+        <PopoverContent>
+          <button className="text-red-500">ログアウト</button>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+};
+
+export default Header;
