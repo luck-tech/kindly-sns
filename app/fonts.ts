626aabd6d0bddc031d66d8d706706aa0ff692a8c
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2025-07-20 21:45:40 +0900

    プロフィールの編集モーダル

---
+++ app/fonts.ts
@@ -0,0 +1,8 @@
+import { Hachi_Maru_Pop } from 'next/font/google';
+
+export const hachiMaruPop = Hachi_Maru_Pop({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-hachi-maru-pop', // CSS変数名を定義
+});
