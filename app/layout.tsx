90d008aeb7b94ff0fd639a9049e67f112228d2f1
ユーザー名 <luck@imai.am>
2025-07-21 14:55:31 +0900

    フォントのexport

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Hachi_Maru_Pop } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 
-const hachiMaruPop = Hachi_Maru_Pop({
+export const hachiMaruPop = Hachi_Maru_Pop({
   weight: "400",
   variable: "--font-hachi-marupop",
   subsets: ["latin"],
