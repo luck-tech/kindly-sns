d88d7c4c46dbc9a21ee5e5b483cf1d0f7f56733d
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-07-25 18:01:06 +0900

    feat: いいねを削除するAPIを作成

--- app/api/posts/[id]/likes/route.ts
+++ app/api/posts/[id]/likes/route.ts
@@ -30,3 +30,36 @@ export async function POST(
     );
   }
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } } // paramsでpostIdを取得
+) {
+  try {
+    const user = await getAuthUser(request); // ログイン中のユーザーを取得
+    if (!user) {
+      // ログイン中のユーザーではなかったらエラーを返す。
+      return NextResponse.json(
+        { message: "認証エラー" },
+        { status: 401 } // 401は「認証されていない」という意味のステータスコード
+      );
+    }
+
+    const sql = "DELETE FROM likes WHERE user_id = $1 AND post_id = $2";
+    const userId = user.userId;
+    const postId = params.id;
+
+    await query(sql, [userId, postId]);
+
+    return NextResponse.json(
+      { message: "いいねを削除しました" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("いいね削除APIでエラー:", error);
+    return NextResponse.json(
+      { message: "サーバー内部でエラーが発生しました" },
+      { status: 500 }
+    );
+  }
+}
