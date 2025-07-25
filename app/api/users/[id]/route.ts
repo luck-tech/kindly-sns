66a3805bbe0aa67f5c88fe325eb2be27a8cb867a
ユーザー名 <luck@imai.am>
2025-07-26 00:34:38 +0900

    パラメータの受け取り方をNext.js15以降のものに変更

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -3,9 +3,9 @@ import { query } from "@/lib/db";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  props: { params: Promise<{ id: string }> }
 ) {
-  const userId = params.id;
+  const { id: userId } = await props.params;
 
   const result = await query(
     `SELECT username, user_id, icon_url FROM users WHERE user_id = $1`,
