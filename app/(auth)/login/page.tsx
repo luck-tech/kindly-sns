2798077ea3504aa9c1e9acdfb42a8fd073a7cede
yuu-1230 <yuto.nagata1230@gmail.com>
2025-07-26 01:18:36 +0900

    タイトルテキスト変更

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
   return (
     <div className="w-full flex px-[160px] py-20">
       <div className="w-full flex flex-col h-[695px] justify-center items-center">
-        <p className="text-[28px] mb-6 text-center">ログイン</p>
+        <p className="text-[28px] mb-6 text-center">おかえりなさい</p>
         <AuthForm type="login" />
         <a href="/signup" className="text-[#827066] mt-4">
           アカウントをお持ちでないですか？ 登録
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -4,7 +4,7 @@ export default function SignupPage() {
   return (
     <div className="w-full flex px-[160px] py-20">
       <div className="w-full flex flex-col h-[695px] justify-center items-center">
-        <p className="text-[28px] mb-6 text-center">アカウントを作成</p>
+        <p className="text-[28px] mb-6 text-center">おかえりなさい</p>
         <AuthForm type="signup" />
         <a href="/login" className="text-[#827066] mt-4">
           既にアカウントをお持ちですか？ ログイン
