27cff4f8f05b78cc9a32a035ffee7dffcf17216d
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-07-25 18:00:37 +0900

    feat: いいねを追加するAPIを作成

---
+++ app/api/posts/[id]/likes/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getAuthUser } from "@/lib/auth";
+import { query } from "@/lib/db";
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { id: string } } // paramsでpostIdを取得
+) {
+  try {
+    const user = await getAuthUser(request); // ログイン中のユーザーを取得
+    if (!user) {
+      // ログイン中のユーザーではなかったらエラーを返す。
+      return NextResponse.json({ message: "認証エラー" }, { status: 401 });
+    }
+    const sql = "INSERT INTO likes (user_id, post_id) VALUES ($1, $2)";
+    const userId = user.userId;
+    const postId = parseInt(params.id, 10);
+
+    await query(sql, [userId, postId]);
+
+    return NextResponse.json(
+      { message: "いいねを追加しました" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("いいね追加APIでエラー:", error);
+    return NextResponse.json(
+      { message: "サーバー内部でエラーが発生しました" },
+      { status: 500 }
+    );
+  }
+}
