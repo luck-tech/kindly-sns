cff0bd45a31ccedac9ebf4bd1519d2331d40e152
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2025-07-27 13:25:11 +0900

    デプロイのエラー解消

--- app/api/users/[id]/posts/route.ts
+++ app/api/users/[id]/posts/route.ts
@@ -3,9 +3,10 @@ import { query } from "@/lib/db";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  props: { params: Promise<{ id: string }> }
 ) {
-  const userId = params.id;
+  const { id: userId } = await props.params;
+
 
   const result = await query(
    `
