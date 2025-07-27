b16cc17b1cc330a5824841d8e0245205068c8cc9
ユーザー名 <luck@imai.am>
2025-07-28 02:00:20 +0900

    layoutリファクタ

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import "../globals.css";
 import { Hachi_Maru_Pop } from "next/font/google";
+import "./globals.css";
 import { ToastContainer } from "react-toastify";
 
 const hachiMaruPop = Hachi_Maru_Pop({
@@ -22,8 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${hachiMaruPop.className} antialiased`}>
-        <ToastContainer />
         {children}
+        <ToastContainer />
       </body>
     </html>
   );
